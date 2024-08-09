9e1b151501ee3a8d05c11500b1572372a1f82ec3
ktkennychow <ktkennychow@gmail.com>
2024-08-09 22:40:16 +0900

    add readiness endpoint

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import "net/http"
+
+func main(){
+	serveMux := http.NewServeMux()
+	server := http.Server{Handler: serveMux, Addr: "localhost:8080" }
+	
+	dir := http.Dir(".")
+	fileServer := http.FileServer(dir)
+
+	serveMux.Handle("/app/*", http.StripPrefix("/app", fileServer))
+
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	server.ListenAndServe()
+}
